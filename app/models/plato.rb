9350885b38ad7feee7bd885c208b6b086ac5443e
jbfermor <jbfermor@hotmail.com>
2021-03-10 23:33:33 +0100

    style platos

--- app/models/plato.rb
+++ app/models/plato.rb
@@ -1,6 +1,6 @@
 class Plato < ApplicationRecord
   has_many :menu_platos, dependent: :destroy
-  has_many :menus, through: :lista_platos
+  has_many :menus, through: :menu_platos
   has_many :plato_ingredientes, dependent: :destroy
   has_many :ingredientes, through: :plato_ingredientes
   has_many :alergenos, through: :ingredientes
