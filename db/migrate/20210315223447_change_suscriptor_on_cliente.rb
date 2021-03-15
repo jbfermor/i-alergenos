9599cfd8a5aab13597d1f0bfd22b833ef3e6d3e0
jbfermor <jbfermor@hotmail.com>
2021-03-16 00:39:26 +0100

    try 4

--- db/migrate/20210315223447_change_suscriptor_on_cliente.rb
+++ db/migrate/20210315223447_change_suscriptor_on_cliente.rb
@@ -1,5 +1,5 @@
 class ChangeSuscriptorOnCliente < ActiveRecord::Migration[6.1]
   def change
-    change_column :clientes, :suscriptor, 'integer USING CAST(suscriptor AS integer)' 
+    change_column :clientes, :suscriptor, using: "suscriptor::integer" 
   end
 end
