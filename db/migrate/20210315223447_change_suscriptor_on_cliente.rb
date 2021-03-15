67533437a387b671ad06d18c4974dc4c2249481e
jbfermor <jbfermor@hotmail.com>
2021-03-16 00:28:28 +0100

    trying to change column type

--- db/migrate/20210315223447_change_suscriptor_on_cliente.rb
+++ db/migrate/20210315223447_change_suscriptor_on_cliente.rb
@@ -1,5 +1,5 @@
 class ChangeSuscriptorOnCliente < ActiveRecord::Migration[6.1]
   def change
-    change_column :clientes, :suscriptor, :integer
+    change_column :clientes, :suscriptor, 'numeric USING CAST(target_column AS numeric)' 
   end
 end
