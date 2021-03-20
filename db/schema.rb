45562bbba13981e8d24a731db25b15170621144c
jbfermor <jbfermor@hotmail.com>
2021-03-20 18:31:02 +0100

    fix 2 to reset db heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_223447) do
+ActiveRecord::Schema.define(version: 2021_03_15_1808807) do
 
   create_table "alergenos", force: :cascade do |t|
     t.string "nombre"
